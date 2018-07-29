c0881a52c610e0c10bd0692c86358aa8d97b067e
堤 啓太 <keita@tsutsuminoMacBook-Air.local>
2018-07-29 23:35:36 +0900

    update

--- Assets/iOSScreenCapture/Scripts/AppStart.cs
+++ Assets/iOSScreenCapture/Scripts/AppStart.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class AppStart : MonoBehaviour
 {
 
     // Use this for initialization
     void Start()
     {
+
+
 #if !UNITY_EDITOR && UNITY_IOS
         UnityiOS.RequestPermissions();
 #endif
+
 	}
+
+
+
 }
+
+
+
---
+++ Assets/iOSScreenCapture/Scripts/download2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class download2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (connect ("http://unipro-school.com/data/ar/0804/img/02"));
+
+	}
+
+
+	IEnumerator connect(string url)
+	{
+		WWW www = new WWW(url);
+		yield return www;
+		RawImage rawImage = GetComponent<RawImage>();
+		rawImage.texture = www.textureNonReadable;
+
+
+	}
+
+}
